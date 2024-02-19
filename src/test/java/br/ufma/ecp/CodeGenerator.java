36607cdda2eff11371baf95443d2777e03ca2d2f
Jessica Santos <jessica.csn@discente.ufma.br>
2024-02-19 14:37:58 -0300

    criação do CodeGenerator

---
+++ src/test/java/br/ufma/ecp/CodeGenerator.java
@@ -0,0 +1,25 @@
+
+package br.ufma.ecp;
+import java.nio.charset.StandardCharsets;
+import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import org.junit.Test;
+
+public class CodeGenerator extends TestSupport {
+
+@Test
+    public void testInt () {
+        var input = """
+            10
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10       
+                    """;
+            assertEquals(expected, actual);
+    }
+
+}
